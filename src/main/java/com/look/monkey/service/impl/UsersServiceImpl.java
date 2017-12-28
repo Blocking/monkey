ada93928a84eb8eb14ed6f350a9228a9c3f78792
blocking <18510855440@sina.cn>
2017-12-28 23:40:11 +0800

    增加应用监控

--- src/main/java/com/look/monkey/service/impl/UsersServiceImpl.java
+++ src/main/java/com/look/monkey/service/impl/UsersServiceImpl.java
@@ -15,7 +15,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
