a58a5d57f25f6ac25a64de4007ff9eccea380071
blocking <18510855440@sina.cn>
2020-05-16 19:43:15 +0800

    删除querydsl-sql

--- src/main/java/com/look/monkey/repository/extend/CustomerExtendRepository.java
+++ src/main/java/com/look/monkey/repository/extend/CustomerExtendRepository.java
@@ -4,9 +4,14 @@ import com.look.monkey.entity.Customer;
 import com.look.monkey.entity.QCustomer;
 import com.look.monkey.repository.Abstract.AbstractRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +22,17 @@ public class CustomerExtendRepository extends AbstractRepository<Customer> {
     private QCustomer qCustomer = QCustomer.customer;
 
     public List<Customer> findAll() {
-        return this.selectFrom(qCustomer).fetch();
+        return this.selectFrom(qCustomer).orderBy(qCustomer.firstName.substring(1,2).castToNum(Integer.class).desc()).fetch();
     }
 
     public Customer findByLastName(String lastName) {
+
+        new Specification<Object>() {
+            @Override
+            public Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder criteriaBuilder) {
+                return null;
+            }
+        };
         return this.selectFrom(qCustomer)
                 .where(qCustomer.lastName.contains(lastName))
                 .fetchFirst();
